b2bca0b0078f679625fdc6a09f117f9588204b49
karimorozova <morozova.karine13@gmail.com>
2022-07-15 10:38:21 +0300

    Create custom Popup

---
+++ tooltip/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <PopupTooltip/>
+</template>
+
+<script>
+import PopupTooltip from './components/PopupTooltip.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PopupTooltip
+}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ tooltip/src/components/PopupTooltip.vue
@@ -0,0 +1,84 @@
+<template lang="pug">
+.tooltip Hello
+    slot
+    .tooltiptext(v-if="tooltipText" :class="side" :style="{backgroundColor: backgroundColor, color: color}") {{ tooltipText }}
+</template>
+
+<script>
+export default {
+  name: 'PopupTooltip',
+  props: {
+    tooltipText: {
+      type: String,
+      default: 'Hi there'
+    },
+    side: {
+      type: String,
+      default: 'top'
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false
+    },
+    backgroundColor: {
+      type: String,
+      default: '#fff'
+    },
+    color: {
+      type: String,
+      default: '#333'
+    },
+
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+.tooltip {
+  position: relative;
+  display: inline-block;
+  width: fit-content;
+
+  &:hover .tooltiptext {
+    visibility: visible;
+  }
+}
+.tooltip .tooltiptext {
+  visibility: hidden;
+  width: fit-content;
+  background-color: white;
+  text-align: center;
+  padding: 7px 15px;
+  border: 1px solid gray;
+  word-break: break-word;
+  white-space: nowrap;
+  border-radius: 2px;
+  //inset: auto auto 0px 0px;
+
+
+  position: absolute;
+  z-index: 1;
+}
+.top {
+  bottom: 100%;
+  right: 50%;
+  transform: translateX(50%);
+  margin-bottom: 10px;
+}
+.right {
+  top: -2px;
+  left: 130%;
+}
+.left {
+  top: -2px;
+  right: 130%;
+}
+.bottom {
+  top: 100%;
+  right: 50%;
+  transform: translateX(50%);
+  margin-top: 10px;
+}
+</style>
